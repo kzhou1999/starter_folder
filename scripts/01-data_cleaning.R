a461e8fced93754e06a2dfd0f9079750aaf74415
kzhou1999 <zqa@zqadeMacBook-Pro.local>
2022-02-05 17:24:30 -0500

    test1

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -13,24 +13,37 @@
 
 #### Workspace setup ####
 # Use R Projects, not setwd().
-library(haven)
 library(tidyverse)
-# Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
-                     )
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
+library(opendatatoronto)
+library(dplyr)
 
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
+# get package
+package <- show_package("0600cad8-d024-483b-a9a8-ecfc3e32e375")
+package
+# get resources
+resources <- list_package_resources("0600cad8-d024-483b-a9a8-ecfc3e32e375")
+data = resources%>%
+  filter(name=="annual-energy-consumption-data-2018")%>%
+  get_resource()
+data<-data%>%
+  filter(!is.na(...6)&!is.na(`Energy Consumption and Greenhouse Gas Emissions Reporting - for 2018`)&!is.na(...8)&!is.na(...10)&!is.na(...12)&!is.na(...32))%>%
+  select(...6,`Energy Consumption and Greenhouse Gas Emissions Reporting - for 2018`,...8,...10,...12,...32,...36)
+annual_energy_comsump_2018<-data%>%
+  mutate(area=as.numeric(data$...6))%>%
+  mutate(operation_type=as.factor(data$...36))%>%
+  mutate(average_hour=as.numeric(data$...8))%>%
+  mutate(use_of_elec=as.numeric(data$...10))%>%
+  mutate(use_of_gas=as.numeric(data$...12))%>%
+  mutate(GHG=as.numeric(data$...32))%>%
+  filter(operation_type=="Fire Stations"|operation_type=="Public Libraries"|operation_type=="Storage Facilities"|operation_type=="Administrative Offices"|operation_type=="Ambulance Stations"|operation_type=="Community Centres")%>%
+  select(area,use_of_elec,use_of_gas,average_hour,GHG,operation_type)
+write.csv(annual_energy_comsump_2018, "inputs/data/annual_energy_comsump_2018.csv")
+
          
 
 #### What's next? ####
 
 
 
+         
          
